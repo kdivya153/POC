bfbdae4075b03a6c8bf6b3df1d5fa184c04d207d
kdivya153 <121901963+kdivya153@users.noreply.github.com>
2024-03-14 12:15:20 -0400

    Create doubleElement.js

---
+++ doubleElement.js
@@ -0,0 +1,7 @@
+//code to double each element in an array
+let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = [];
+for (let i = 0; i < numbers.length; i++) {
+    doubledNumbers.push(numbers[i] * 2);
+}
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
