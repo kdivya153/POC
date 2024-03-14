950167fcd93490166178c191ecc6e546d341d311
kdivya153 <121901963+kdivya153@users.noreply.github.com>
2024-03-14 13:02:34 -0400

    Create calculateArea.js

---
+++ calculateArea.js
@@ -0,0 +1,13 @@
+function calculateArea(radius) {
+    const pi = 3.14;
+    const area = pi * radius * radius;
+    return area;
+}
+
+function calculatePerimeter(l) {
+    const area = 2 * l + b
+    return area;
+}
+
+const radius = 5;
+const area = calculateArea(radius);
